44bc41433908ff15ed2745013e3602733dbdfb3d
Izabelle Santos do Nascimento <izabelle.sdn@gmail.com>
2022-11-09 21:42:53 -0300

    função botão

--- project/js/filter-bar-map.js
+++ project/js/filter-bar-map.js
@@ -1,13 +1,17 @@
 //get section and buttons 
 const pointBtn = document.querySelector(".point-btn");
 const filterBarContainer = document.querySelector(".filter-bar-container");
- 
+
 pointBtn.addEventListener('click', () => {
-    if (filterBarContainer.classList.contains(".hide-filter-bar")) {
-        pointBtn.classList.remove(".hide-filter-bar");
-        pointBtn.classList.add(".show-filter-bar");
+    if (filterBarContainer.classList.contains("hide-filter-bar")) {
+        filterBarContainer.style.overflow = 'hidden';
+        filterBarContainer.style.height = '0px';
+        filterBarContainer.style.transition = '0.5s'
+        filterBarContainer.classList.remove('hide-filter-bar')
+        filterBarContainer.classList.remove('hide')
     } else {
-        pointBtn.classList.remove(".show-filter-bar");
-        pointBtn.classList.add(".hide-filter-bar");
+        filterBarContainer.style.overflow = 'visible';
+        filterBarContainer.style.height = '50px';
+        filterBarContainer.classList.add('hide-filter-bar')
     }
 })
